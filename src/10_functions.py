ad8175672a33b3ace53b5c1d181c8af1d2840fa9
chrispetito <christopherpetito@gmail.com>
2019-09-04 11:06:46 -0700

    change if else to ternary in functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,11 +1,11 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+
+
 def is_even(num):
-    if num % 2 == 0:
-        return True
-    else:
-        return False
+    True if num % 2 == 0 else False
+
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -14,8 +14,4 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-if (num % 2 == 0) :
-    print('Even!')
-else : 
-    print('Odd!')
-
+print('Even!') if num % 2 == 0 else print('Odd!')
